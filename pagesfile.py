210f87d68ffa17dda454667dcd7eb5fc9d7ec0b4
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2014-01-25 01:30:02 +0000

    Working on dynamic page allocation

--- pagesfile.py
+++ pagesfile.py
@@ -13,7 +13,7 @@ class PagesFile(object):
 		else:
 			self.handle = fi
 
-		self.method = "bz2"
+		self.method = "bz2 "
 		self.virtualCursor = 0
 
 		self.pageIndex = [] #Index of on disk pages
@@ -22,8 +22,11 @@ class PagesFile(object):
 		self.pagesPlain = []
 		self.pagesMeta = []
 		self.pagesChanged = []
+		self.pageTrash = []
 
-		self.headerStruct = struct.Struct(">QQQ")
+		#inUse, uncompSize, compSize, uncompPos, allocSize
+		self.headerStruct = struct.Struct(">BQQQQ")
+
 		self.footerStruct = struct.Struct(">Q")
 		
 		self.plainLen = 0
@@ -46,10 +49,11 @@ class PagesFile(object):
 		self.handle.seek(4)
 		self.handle.write(struct.pack(">Q", self.plainLen))
 
-		for plain, meta, changed in zip(self.pagesPlain, self.pagesMeta, self.pagesChanged):
+		for i, changed in enumerate(self.pagesChanged):
 			if not changed:
 				continue
-			
+			self._write_page_to_disk(i)
+
 
 	def write(self, data):
 		#http://www.skymind.com/~ocrow/python_string/
@@ -84,7 +88,7 @@ class PagesFile(object):
 
 	def _add_page(self, pos, plainLen):
 		self.pagesMeta.append({'pagePos': None, 'compSize': None, 'uncompPos': pos,
-			 'uncompSize': plainLen, 'method': self.method})
+			 'uncompSize': plainLen, 'method': self.method, 'allocSize': None})
 		self.pagesChanged.append(True)
 		self.pagesPlain.append(bytearray("".join("\x00" for i in range(plainLen))))
 
@@ -95,16 +99,18 @@ class PagesFile(object):
 
 		self.plainLen = struct.unpack(">Q", self.handle.read(8))[0]
 		self.pageAllocLen = 0
-
 		self.pageIndex = []
+		self.pageTrash = []
 		while True:
 			meta = self._parse_header_at_cursor()
 			if meta is None:
 				break
-			print meta
-			self.pageIndex.append(meta)
-			pos = self.handle.tell()
-			self.handle.seek(pos + meta['compSize'])
+			print "meta", meta
+			if meta['inUse']:
+				self.pageIndex.append(meta)
+			else:
+				self.pageTrash.append(meta)
+			self.handle.seek(meta['allocSize'], 1)
 			footerData = self.handle.read(8)
 			endStr = self.handle.read(4)
 			if endStr != "pend":
@@ -119,10 +125,10 @@ class PagesFile(object):
 			raise Exception("File format not recognised")
 
 		header = self.handle.read(self.headerStruct.size)
-		uncompSize, compSize, uncompPos = self.headerStruct.unpack(header)
+		inUse, uncompSize, compSize, uncompPos, allocSize = self.headerStruct.unpack(header)
 		method = self.handle.read(4)
-		return {'pagePos': pagePos, 'compSize': compSize, 'uncompPos': uncompPos,
-			 'uncompSize': uncompSize, 'method': method}
+		return {'inUse': inUse, 'pagePos': pagePos, 'compSize': compSize, 'uncompPos': uncompPos,
+			 'uncompSize': uncompSize, 'method': method, 'allocSize': allocSize}
 
 	def _get_page_for_index(self, pos):
 
@@ -133,6 +139,7 @@ class PagesFile(object):
 
 		#Seek for suitable page on disk
 		for i, page in enumerate(self.pageIndex):
+			#print "check", page
 			if pos >= page['uncompPos'] and pos < page['uncompPos'] + page['uncompSize']:
 				return self._load_page_into_mem(page)
 
@@ -144,7 +151,7 @@ class PagesFile(object):
 
 		if meta['method'] == "bz2 ":
 			plainData = bytearray(bz2.decompress(binData))
-			if len(self.cursorPagePlain) != meta['uncompSize']:
+			if len(plainData) != meta['uncompSize']:
 				raise Exception("Extracted data has incorrect length")
 
 			self.pagesPlain.append(plainData)
@@ -156,52 +163,114 @@ class PagesFile(object):
 			raise Exception("Not implemented")
 
 	def read(self, bytes):
-		pass
+		pageNum = self._get_page_for_index(self.virtualCursor)
 
+		if pageNum is None:
+			raise Exception("Undefined location in file")
+
+		plain = self.pagesPlain[pageNum]
+		meta = self.pagesMeta[pageNum]
+		
+		localCursor = self.virtualCursor - meta['uncompPos']
+		bytesRemain = meta['uncompSize'] - localCursor
+		if bytesRemain < bytes:
+			bytes = bytesRemain
 			
+		return plain[localCursor:localCursor+bytes]
 
 	def tell(self):
 		return self.virtualCursor
 
-	def seek(self, pos):
-		if self.mode == "w":
-			raise Exception("Seeking in write mode not supported")
+	def seek(self, pos, mode=0):
+		if mode != 0:
+			raise Exception("Not implemented")
 
 		self.virtualCursor = pos
 
 	def __len__(self):
 		return self.plainLen
 
-	def _write_page(self, i):
-		if self.method == "bz2":
-			return self._write_page_bz2(data)
-		raise Exception("Not implemented compression:" + self.method)
+	def _write_page_to_disk(self, i):
 
-	def _write_page_bz2(self, i):
+		plain = self.pagesPlain[i]
+		meta = self.pagesMeta[i]
+
+		if meta['method'] != "bz2 ":
+			raise Exception("Not implemented compression:" + meta['method'])
+
 		import bz2
-		compressedData = bz2.compress(data)
-		print "Write page", len(data), ", compressed size", len(compressedData)
+		encodedData = bz2.compress(plain)
+		
+		#Decide where to write
+		if meta['pagePos'] is None:
+			print "Write new page at end of file"
+			self.handle.seek(0, 2) #Write at end
+			meta['pagePos'] = self.handle.tell()
+			meta['allocSize'] = len(encodedData)
+		else:
+			#Does this fit in original location
+			if len(encodedData) <= meta['compSize']:
+				pass
+				print "Write page at existing position"
+
+			else:
+				print "Write existing page at end of file"
+
+				#Free old location
+				self._set_page_unused(meta)
+
+				#Write at end of file
+				self.handle.seek(0, 2)
+				meta['pagePos'] = self.handle.tell()
+				meta['allocSize'] = len(encodedData)
+
+		meta['compSize'] = len(encodedData)
+
+		if meta['method'] == "bz2 ":
+			self._write_page_bz2(meta, plain, encodedData)
+
+		self.pagesChanged[i] = False
+
+	def _set_page_unused(self, meta):
+		print "Set page to unused"
 
 		#Header
 		self.handle.write("page")
-		header = self.headerStruct.pack(len(data), len(compressedData), self.writtenPlainBytes)
+		header = self.headerStruct.pack(0x00, 0, 0, 0, meta['allocSize'])
+		self.handle.write(header)
+		self.handle.write("bz2 ")
+
+		#Leave footer unchanged
+
+	def _write_page_bz2(self, meta, data, encoded):
+
+		self.handle.seek(meta['pagePos'])
+		print "Write page", len(data), ", compressed size", len(encoded)
+
+		#Header
+		self.handle.write("page")
+		header = self.headerStruct.pack(0x01, meta['uncompSize'], meta['compSize'], meta['uncompPos'], meta['allocSize'])
 		self.handle.write(header)
 		self.handle.write("bz2 ")
 
 		#Copy data
-		self.handle.write(compressedData)
-		self.writtenPlainBytes += len(data)
+		self.handle.write(encoded)
 
 		#Footer
-		footer = self.footerStruct.pack(len(compressedData))
+		footer = self.footerStruct.pack(meta['allocSize'])
 		self.handle.write(footer)
 		self.handle.write("pend")
 
 if __name__ == "__main__":
 
 	pf = PagesFile("test.pages")	
-	pf.write("stuff")
-	pf.write("more")
+	pf.write("stuffandmorestuffxxxx")
+	pf.seek(0)
+	print "readback", pf.read(5)
+
+	pf.seek(1500000)
+	pf.write("foo")
+
 	pf.flush()
 	print "len", len(pf)
 
